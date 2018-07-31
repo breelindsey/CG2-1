07957cddac39c92bb639838d984004728caa71a1
breelindsey <35509667+breelindsey@users.noreply.github.com>
2018-07-30 22:30:15 -0500

    One day I'll get how this works.

---
+++ CG2-1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CG2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, I'm Hello World! What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello" + " " + name);
+            Console.ReadLine();
+                
+        }
+    }
+}
